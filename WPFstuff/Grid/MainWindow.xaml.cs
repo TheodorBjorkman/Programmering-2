52e2c96f9375e0620c65055f603e2746f30e8fe6
TheodorBjorkman <70270703+TheodorBjorkman@users.noreply.github.com>
2021-10-06 20:12:49 +0200

    WPF stuff
    saker för wpf inför prov

---
+++ WPFstuff/Grid/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFstuff
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
+/*
+For MainWindow.xaml
+
+Grid sizes are:
+int = amount of px
+Auto = necessary space
+* = all space possible
+
+
+Each individual column/row has to be defined in the relevant Grid.Row/ColumnDefinition
+
+*/
---
+++ WPFstuff/SP/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace SP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            testBlock.Text = "Test";
+        }
+
+        private void TestButton(object sender, RoutedEventArgs e)
+        {
+            string input = testBox.Text;
+            MessageBox.Show(input);
+            testBlock.Text = input;
+        }
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.Multiselect = true;
+            openFileDialog.InitialDirectory = @"c:\";
+			if(openFileDialog.ShowDialog() == true)
+            {
+				foreach(string filename in openFileDialog.FileNames)
+					txtBox.Items.Add(System.IO.Path.GetFileName(filename));
+            }
+		}
+    }
+}
